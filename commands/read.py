600b5349a951a1a1b6f33f3e910bc3a46a1c8bbe
Joao Pedro Leal <jpkleal@gmail.com>
2024-09-13 19:24:34 -0300

    fiz python input

--- commands/read.py
+++ commands/read.py
@@ -7,12 +7,17 @@ class Read:
         VarTypes.TEXT: "%s",
         VarTypes.BOOL: "%d"
     }
+    vartype_to_py = {
+        VarTypes.NUM: "float",
+        VarTypes.TEXT: "str",
+        VarTypes.BOOL: "bool"
+    }
 
     def __init__(self):
         self.assign_id = None
 
     def to_py(self):
-        code = f"{self.assign_id.name} = input()"
+        code = f"{self.assign_id.name} = {self.vartype_to_py[self.assign_id.var_type]}(input())"
         return code
 
     def to_c(self):
